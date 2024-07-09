74ad0d05071eea8d67f5cc256abcb6676840c35c
Jhon <jhowjhoe@icloud.com>
2024-07-09 03:57:32 -0300

    Update soap.js

--- config/soap.js
+++ config/soap.js
@@ -1,5 +1,7 @@
 const soap = require('soap');
 const fs = require('fs');
+const { exec } = require('child_process');
+
 const mode = process.env.API_MODE;
 let url;
 
@@ -10,6 +12,8 @@ switch (mode) {
     case "2":
         url = process.env.BHISS_URL_DEV;
         break;
+    default:
+        throw new Error(`Mode ${mode} not supported.`);
 }
 
 class Client {
@@ -18,11 +22,36 @@ class Client {
     }
 
     setClient() {
+        // Caminhos para os arquivos PFX e PEM
+        const pfxFile = process.env.PATH_CERT;
+        const pemFile = 'certs/certificado.pem'; // Caminho onde o arquivo PEM será salvo
+
+        // Converter PFX para PEM se necessário
+        this.convertPfxToPem(pfxFile, pemFile);
+    }
+
+    convertPfxToPem(pfxFile, pemFile) {
+        const command = `openssl pkcs12 -in ${pfxFile} -out ${pemFile} -nodes -passin pass:${process.env.PWD_CERT}`;
+
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error('Error converting PFX to PEM:', error);
+                return;
+            }
+            console.log('PFX converted to PEM successfully.');
+            // Depois de converter, configure o cliente SOAP com o arquivo PEM
+            this.setSoapClient(pemFile);
+        });
+    }
+
+    setSoapClient(pemFile) {
         soap.createClient(url, {
             wsdl_options: {
                 pfx: fs.readFileSync(process.env.PATH_CERT),
-                passphrase: process.env.PWD_CERT,
-                strictSSL: true,
+                cert: fs.readFileSync(pemFile), // Certificado PEM
+                key: fs.readFileSync(pemFile), // Chave privada PEM
+                passphrase: process.env.PWD_CERT, // Senha do certificado
+                strictSSL: true
             }
         }, (error, client) => {
             if (error) {
