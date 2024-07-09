15f806ab2e42cbb13088d9e539307cd8907ebc1d
Jhon <jhowjhoe@icloud.com>
2024-07-09 04:10:29 -0300

    Update soap.js

--- config/soap.js
+++ config/soap.js
@@ -1,43 +1,45 @@
 const soap = require('soap');
 const fs = require('fs');
-const mode = process.env.API_MODE;
+const mode = process.env.API_MODE ;
 let url;
 
 switch (mode) {
     case "1":
-        url = process.env.BHISS_URL_PROD;
+        url = process.env.BHISS_URL_PROD
         break;
     case "2":
-        url = process.env.BHISS_URL_DEV;
+        url = process.env.BHISS_URL_DEV
         break;
-    default:
-        throw new Error(`Mode ${mode} not supported.`);
 }
 
+
 class Client {
-    constructor() {
-        this.client = null;
+    
+    constructor(){ 
+        this.client = null
     }
 
-    setClient() {
+    setClient(){
         soap.createClient(url, {
             wsdl_options: {
-                cert: fs.readFileSync('./certs/certificado.pem'), // Certificado PEM
-                key: fs.readFileSync('./certs/chave_privada.pem'), // Chave privada PEM
                 pfx: fs.readFileSync(process.env.PATH_CERT),
                 passphrase: process.env.PWD_CERT,
-                strictSSL: true
+                strictSSL: true 
             }
         }, (error, client) => {
-            if (error) {
-                console.error('Error creating SOAP client:', error);
-                return;
+            
+            if(error) {
+                console.log(error);
+                return error;
             }
 
+            client.setSecurity(new soap.ClientSSLSecurityPFX(process.env.PATH_CERT,  process.env.PWD_CERT));
             this.client = client;
-            console.log(`SOAP ${mode == 1 ? 'Production' : 'Dev'} Connected - ${url}`);
-        });
+
+            console.log(`SOAP ${ mode == 1 ? 'Production' : 'Dev' } Connected - ${url}`)
+        })
     }
+
 }
 
 module.exports = new Client();
