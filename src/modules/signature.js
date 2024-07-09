ccd2c8206a53d573352f009975763d6ad60f0a0e
Jhon <jhowjhoe@icloud.com>
2024-07-09 05:08:18 -0300

    ff

--- src/modules/signature.js
+++ src/modules/signature.js
@@ -1,62 +1,39 @@
 const { SignedXml } = require('xml-crypto');
 const fs = require('fs');
 const pem = require('pem');
-const myKeyInfo = require('../providers/myKeyInfo');
+const myKeyInfo = require('../providers/myKeyInfo')
+
 
 class Signature {
-    constructor() {
-        // Carrega o certificado PEM e a chave privada
+
+    constructor(){
         this.certificado = fs.readFileSync(process.env.PATH_CERT_PEM);
-        this.chavePrivada = fs.readFileSync(process.env.PATH_CERT_KEY);
     }
 
-    sign(xml, tag) {
-        return new Promise((resolve, reject) => {
-            pem.readPkcs12(process.env.PATH_CERT, { p12Password: process.env.PWD_CERT }, (err, cert) => {
-                if (err) {
-                    console.error("Error reading PKCS12:", err);
-                    reject(err);
-                    return;
-                }
+    sign(xml, tag){
 
-                // Obtém o certificado no formato correto para SignedXml
-                const certificado = cert.cert.toString()
-                    .replace('-----BEGIN CERTIFICATE-----', '').trim()
-                    .replace('-----END CERTIFICATE-----', '').trim()
-                    .replace(/(\r\n\t|\n|\r\t)/gm, "");
+       return new Promise((resolve, reject) => {
 
-                // Configuração das opções de assinatura
-                const options = {
-                    implicitTransforms: ["http://www.w3.org/TR/2001/REC-xml-c14n-20010315"]
-                };
+        pem.readPkcs12(process.env.PATH_CERT, { p12Password: process.env.PWD_CERT }, (err, cert) => {
 
-                // Inicializa o SignedXml com as opções
-                const sig = new SignedXml(null, options);
+            if(err){
+                reject(err)
+                return
+            }
+            
+            const certificado = cert.cert.toString().replace('-----BEGIN CERTIFICATE-----', '').trim().replace('-----END CERTIFICATE-----', '').trim().replace(/(\r\n\t|\n|\r\t)/gm,"");
+            const option = {implicitTransforms: ["http://www.w3.org/TR/2001/REC-xml-c14n-20010315"]}
+            const sig = new SignedXml(null, option);
+            sig.addReference(`//*[local-name(.)='${tag}']`, ['http://www.w3.org/2000/09/xmldsig#enveloped-signature', 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'])  
+            sig.signingKey = this.certificado;
+            sig.keyInfoProvider = new myKeyInfo(certificado);
+            sig.computeSignature(xml, { location: {reference: `//*[local-name(.)='${tag}']`, action: 'after'}})
+            resolve(sig.getSignedXml())
+        })
 
-                // Adiciona a referência do XML que será assinado
-                sig.addReference(`//*[local-name(.)='${tag}']`, [
-                    'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
-                    'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
-                ]);
-
-                // Define a chave privada e o provedor de informações da chave
-                sig.signingKey = this.chavePrivada;
-                sig.keyInfoProvider = new myKeyInfo(certificado);
-
-                try {
-                    console.log("Computing signature...");
-                    // Calcula a assinatura do XML
-                    sig.computeSignature(xml, { location: { reference: `//*[local-name(.)='${tag}']`, action: 'after' } });
-                    console.log("Signature computed successfully.");
-                    // Retorna o XML assinado
-                    resolve(sig.getSignedXml());
-                } catch (error) {
-                    console.error("Error computing signature:", error);
-                    reject(error);
-                }
-            });
-        });
+       })
     }
+
 }
 
 module.exports = new Signature();
