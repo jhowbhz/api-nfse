3f939bd89da78cd8df81ee28fb7fbc6b004eb027
Jhon <jhowjhoe@icloud.com>
2024-07-09 03:39:07 -0300

    Update signature.js

--- src/modules/signature.js
+++ src/modules/signature.js
@@ -5,6 +5,7 @@ const myKeyInfo = require('../providers/myKeyInfo');
 
 class Signature {
     constructor() {
+        // Carrega o certificado PEM e a chave privada
         this.certificado = fs.readFileSync(process.env.PATH_CERT_PEM);
         this.chavePrivada = fs.readFileSync(process.env.PATH_CERT_KEY);
     }
@@ -18,25 +19,36 @@ class Signature {
                     return;
                 }
 
+                // Obtém o certificado no formato correto para SignedXml
                 const certificado = cert.cert.toString()
                     .replace('-----BEGIN CERTIFICATE-----', '').trim()
                     .replace('-----END CERTIFICATE-----', '').trim()
                     .replace(/(\r\n\t|\n|\r\t)/gm, "");
 
-                const option = { implicitTransforms: ["http://www.w3.org/TR/2001/REC-xml-c14n-20010315"] };
-                const sig = new SignedXml(null, option);
+                // Configuração das opções de assinatura
+                const options = {
+                    implicitTransforms: ["http://www.w3.org/TR/2001/REC-xml-c14n-20010315"]
+                };
+
+                // Inicializa o SignedXml com as opções
+                const sig = new SignedXml(null, options);
+
+                // Adiciona a referência do XML que será assinado
                 sig.addReference(`//*[local-name(.)='${tag}']`, [
                     'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
                     'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
                 ]);
 
+                // Define a chave privada e o provedor de informações da chave
                 sig.signingKey = this.chavePrivada;
                 sig.keyInfoProvider = new myKeyInfo(certificado);
 
                 try {
                     console.log("Computing signature...");
+                    // Calcula a assinatura do XML
                     sig.computeSignature(xml, { location: { reference: `//*[local-name(.)='${tag}']`, action: 'after' } });
                     console.log("Signature computed successfully.");
+                    // Retorna o XML assinado
                     resolve(sig.getSignedXml());
                 } catch (error) {
                     console.error("Error computing signature:", error);
