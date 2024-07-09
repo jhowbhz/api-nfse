9d2bdfb0fd2233d29e89bd9abc4ccb23fb85d533
Jhon <jhowjhoe@icloud.com>
2024-07-09 03:52:07 -0300

    Update soap.js

--- config/soap.js
+++ config/soap.js
@@ -1,7 +1,5 @@
 const soap = require('soap');
 const fs = require('fs');
-const { exec } = require('child_process');
-
 const mode = process.env.API_MODE;
 let url;
 
@@ -12,8 +10,6 @@ switch (mode) {
     case "2":
         url = process.env.BHISS_URL_DEV;
         break;
-    default:
-        throw new Error(`Mode ${mode} not supported.`);
 }
 
 class Client {
@@ -22,36 +18,12 @@ class Client {
     }
 
     setClient() {
-        // Caminhos para os arquivos PFX e PEM
-        const pfxFile = process.env.PATH_CERT;
-        const pemFile = 'certs/certificado.pem'; // Caminho onde o arquivo PEM será salvo
-
-        // Converter PFX para PEM se necessário
-        this.convertPfxToPem(pfxFile, pemFile);
-    }
-
-    convertPfxToPem(pfxFile, pemFile) {
-        const command = `openssl pkcs12 -in ${pfxFile} -out ${pemFile} -nodes -passin pass:${process.env.PWD_CERT}`;
-
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error('Error converting PFX to PEM:', error);
-                return;
-            }
-            console.log('PFX converted to PEM successfully.');
-            // Depois de converter, configure o cliente SOAP com o arquivo PEM
-            this.setSoapClient(pemFile);
-        });
-    }
-
-    setSoapClient(pemFile) {
         soap.createClient(url, {
             wsdl_options: {
-                cert: fs.readFileSync(pemFile),
-                key: fs.readFileSync(pemFile),
+                pfx: fs.readFileSync(process.env.PATH_CERT),
                 passphrase: process.env.PWD_CERT,
                 strictSSL: true,
-                secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_TLSv1 // Evita SSLv3 e TLSv1
+                secureOptions: require('constants').SSL_OP_NO_SSLv3 | require('constants').SSL_OP_NO_TLSv1 // Evita SSLv3 e TLSv1
             }
         }, (error, client) => {
             if (error) {
